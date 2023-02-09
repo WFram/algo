871495cb2fc5be87a19750b76d578c8f6748ad28
WFram <earl.freedom.ea@gmail.com>
2023-02-09 17:39:26 +0300

    Add solution to "Palindrome number" problem [2]

--- src/palindrome_number.cpp
+++ src/palindrome_number.cpp
@@ -2,8 +2,8 @@
 // Created by wfram on 2/9/23.
 //
 
-#include <string>
 #include <iostream>
+#include <string>
 
 // https://leetcode.com/problems/palindrome-number/
 class Solution {
@@ -14,10 +14,38 @@ class Solution {
 class NormalSolution : public Solution {
  public:
   bool isPalindrome(int x) override {
+    if (x < 0) return false;
     auto forward = std::to_string(x);
     auto backward = std::to_string(x);
     std::reverse(backward.begin(), backward.end());
     return (forward == backward);
+  }
+};
+
+class FastSolution : public Solution {
+ public:
+  bool isPalindrome(int x) override {
+    int a = x, b;
+    long long int rev{0};
+    do {
+      b = std::abs(a) % 10;
+      rev *= 10;
+      rev += b;
+      a /= 10;
+    } while (a != 0);
+
+    return (rev == x);
+  }
+};
+
+class MemoryEfficientSolution : public Solution {
+ public:
+  bool isPalindrome(int x) override {
+    if (x < 0) return false;
+    std::string s = std::to_string(x);
+    for (int i = 0; i < s.size() / 2 + 1; i++)
+      if (s[i] != s[s.size() - 1 - i]) return false;
+    return true;
   }
 };
 
